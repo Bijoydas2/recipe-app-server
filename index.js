66e9963214183dc4bdf769876da20d51acc20543
bijoy133 <bijoydas5680@!gmail.com>
2025-05-21 17:50:34 +0600

    recipes 6 data get

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+
 require('dotenv').config();
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const app= express();
+const app = express();
 const port = process.env.PORT || 3000;
-// midlleware
+
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uevarui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,31 +20,49 @@ const client = new MongoClient(uri, {
   }
 });
 
-
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const db = client.db('recipeDB');
+    const recipeCollection = db.collection('recipes');
+
+    app.get('/recipes', async (req, res) => {
+   
+        const recipes = await recipeCollection
+          .find()
+          .sort({ likes: -1 })
+          .limit(6)
+          .toArray();
+        res.send(recipes);
+      
+    });
+
+
+    app.post('/recipes', async (req, res) => {
+
+    const recipe = req.body;
+    recipe.likes = 0;
+    const result = await db.collection('recipes').insertOne(recipe);
+    res.send(result)
+
+   });
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+  } catch (error) {
+    console.error(error);
   }
 }
+
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
-
-
-
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port, ${port}`)
-})
+  console.log(`🚀 Server running on port ${port}`);
+});
