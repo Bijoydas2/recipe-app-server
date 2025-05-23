c8551d8ef1359f58224a77b0be2d353ff155b3a9
bijoy133 <bijoydas5680@!gmail.com>
2025-05-24 02:49:13 +0600

    recheaked server side

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db('recipeDB');
     const recipeCollection = db.collection('recipes');
 
@@ -102,7 +102,7 @@ app.put("/api/recipes/:id", async (req, res) => {
 
 
 
-   await client.db("admin").command({ ping: 1 });
+  //  await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (error) {
     console.error(error);
